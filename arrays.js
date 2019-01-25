3254bb5a53d70bd04acf90ef23930f30b9e0bece
Preston Elliott <preston.elliott24@gmail.com>
2019-01-25 23:41:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  return ...array.unshift("mars");
+  return ...array.unshift(element);
   }
   
 function destructivelyAddElementToBeginningOfArray(array, element) {
